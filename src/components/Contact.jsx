f3acd4b411210f618d94d021fc9bd7c059842b03
Jacob Deepu <47151840+JacobDeepu@users.noreply.github.com>
2023-06-24 11:39:31 +0530

    DNM

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,12 +1,21 @@
 import React from "react";
+import { TextField,Button } from '@mui/material'
 import "./Contact.css";
 function Contact() {
     return (
+        <>
         <div className="contact">
             <div className="contacthead">
                 <h1>contact</h1>
             </div>
         </div>
+        <br />
+        <TextField variant='outlined' label='Name' /><br /><br />
+        <TextField variant='outlined' label='Email' /><br /><br /> 
+        <TextField variant='outlined' label='Number' /><br /><br /> 
+        <TextField variant='outlined' label='Message' /><br /><br />  
+        <Button variant='contained' color='secondary'>Contact</Button>
+        </>
     );
 }
 
